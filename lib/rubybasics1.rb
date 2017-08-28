f57ac8b4294a3a7429a8936ab6611ff48bdd173d
citrusspoon <citrusspoon@gmail.com>
2017-08-28 20:32:01 +0000

    Completed Part II method

--- lib/rubybasics1.rb
+++ lib/rubybasics1.rb
@@ -10,24 +10,38 @@ end
 
 # Part II
 def max_2_sum arr
-  if arr.length == 0
+  if arr.empty?
     return 0
   elsif arr.length == 1
     return arr[0]
   else
-    largest = 0
+    arr.sort!
+    return arr[-1] + arr[-2]
+  end
+    
+    
+    
+    
+    
+    
+end
+
+
+
+
+=begin
+largest = 0
     secondLargest = 0
     arr.each do |i|
       if arr[i] > largest
         largest = arr[i]
-        elsif arr[i] > secondLargest
+      elsif arr[i] > secondLargest
         secondLargest = arr[i]
       end
     end
     return largest + secondLargest
-  end
-    
-end
+
+=end
 
 # Part III
 def sum_to_n? arr, n
