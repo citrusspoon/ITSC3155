71f5d61eff64a0650c6fd30ecdc20c74d4d51f27
citrusspoon <citrusspoon@gmail.com>
2017-08-28 20:15:23 +0000

    Added Part II method. Doesn't work yet

--- lib/rubybasics1.rb
+++ lib/rubybasics1.rb
@@ -10,7 +10,23 @@ end
 
 # Part II
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  else
+    largest = 0
+    secondLargest = 0
+    arr.each do |i|
+      if arr[i] > largest
+        largest = arr[i]
+        elsif arr[i] > secondLargest
+        secondLargest = arr[i]
+      end
+    end
+    return largest + secondLargest
+  end
+    
 end
 
 # Part III
