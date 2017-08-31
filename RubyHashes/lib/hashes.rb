6f5d5c0b46df7c7fa0575d55f40a7d2f4645fd60
citrusspoon <citrusspoon@gmail.com>
2017-08-31 18:43:09 +0000

    Finished Part II array2d_2_hash

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -8,7 +8,7 @@ def array_2_hash emails, contacts
         return contacts
     else
         contacts.each do |key, value|
-            String key2 = :"#{key}"
+            key2 = :"#{key}"
             contacts2[key2] = emails[x]
             x+=1
         end
@@ -20,7 +20,23 @@ end
 
 # Part II
 def array2d_2_hash contact_info, contacts
-    # YOUR CODE HERE
+    
+    contacts2 = {}
+    x = 0
+    
+    if contact_info[0][0] == nil
+        return contacts
+    end
+    
+    
+    contacts.each do |key, value|
+        key2 = :"#{key}"
+        contacts2[key2] = {:email => contact_info[x][0], :phone => contact_info[x][1]}
+        
+        x+=1
+    end
+    return contacts2
+    
 end
 
 # Part III
