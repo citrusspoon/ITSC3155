75652d4b811fd0fbaeec8df52267e6f598eb0891
citrusspoon <citrusspoon@gmail.com>
2017-09-06 20:00:17 +0000

    Finished level 3

--- rubywarrior/bill-beginner/player.rb
+++ rubywarrior/bill-beginner/player.rb
@@ -1,8 +1,14 @@
 class Player
   def play_turn(warrior)
-    
-    if warrior.feel.empty?
-       warrior.walk!
+    if warrior.health < 10
+      warrior.rest!
+    elsif warrior.feel.empty?
+         
+      if warrior.health < 20
+        warrior.rest!
+      else
+        warrior.walk!
+      end
     else
       warrior.attack!
     end
