7bfc7bee734ebb6ff11ba03e81112f27be4187e6
citrusspoon <citrusspoon@gmail.com>
2017-09-06 20:39:03 +0000

    Finished level 4

--- rubywarrior/bill-beginner/player.rb
+++ rubywarrior/bill-beginner/player.rb
@@ -1,16 +1,23 @@
 class Player
+  
+   def initialize
+    @health = 20
+   end
+  
   def play_turn(warrior)
-    if warrior.health < 10
-      warrior.rest!
-    elsif warrior.feel.empty?
-         
+    
+    if warrior.feel.empty? && @health <= warrior.health #empty and not being attacked
       if warrior.health < 20
         warrior.rest!
-      else
+      else 
         warrior.walk!
       end
+    elsif warrior.feel.empty? && @health > warrior.health
+      warrior.walk!
     else
       warrior.attack!
+
     end
+    @health = warrior.health
   end
 end
