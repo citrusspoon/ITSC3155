fdf5444fd7725e40ac972a205a8f5ccb39cce812
citrusspoon <citrusspoon@gmail.com>
2017-09-14 00:06:21 +0000

    Finished part II

--- ruby_calisthenics1/lib/fun_with_strings.rb
+++ ruby_calisthenics1/lib/fun_with_strings.rb
@@ -11,9 +11,25 @@ module FunWithStrings
     
     str = self
     countHash = Hash.new
+    word = ""
     
-    str = ((str.gsub(/\W/,"").gsub(/\d/,"")).downcase)
+    str = str.downcase
+    str = str.gsub(/\s+/, " ").gsub(/[^a-z\n\s]/, "")
+    str << " "
+    str.each_char do |i|
     
+     if (i =~ /[a-z]/)
+        word << i
+      elsif(countHash.key?(word))
+        countHash[word] += 1
+        word = ""
+      elsif(!word.eql?(""))
+        countHash[word] = 1
+        word = ""
+      end
+    end
+    
+    return countHash
     
     
   end
